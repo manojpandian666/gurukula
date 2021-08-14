5ab739ef1c420ed9b0a880f203e8031676e701e2
Manoj Kumar <75692359+manojpandian666@users.noreply.github.com>
2021-08-14 16:28:30 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function headerFun() {
+    var x = document.getElementById("headButton");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+  }
